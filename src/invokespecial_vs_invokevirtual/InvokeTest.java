b872e0da277a181781865058f26e61f936741aaa
Administrator <Administrator@AFODC-611081928>
2016-12-09 23:00:46 +0800

    编码处理

--- src/invokespecial_vs_invokevirtual/InvokeTest.java
+++ src/invokespecial_vs_invokevirtual/InvokeTest.java
@@ -4,8 +4,8 @@ public class InvokeTest {
    public static void main( String args[] ) {  
 	   
 	      SubClass5 b = new SubClass5();  
-	      SuperClass5 supper = b;              //向上转型引用  
-	      System.out.println(supper.method());//invokevirtual，当前引用的对象是b  
+	      SuperClass5 supper = b;              //鍚戜笂杞瀷寮曠敤  
+	      System.out.println(supper.method());//invokevirtual锛屽綋鍓嶅紩鐢ㄧ殑瀵硅薄鏄痓  
 	      supper.method();
 	      b.subMethod();  
 	      b.otherMethod();  
