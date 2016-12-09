9b76c88b29eb53658a75830610fc155c8248ad7f
Administrator <Administrator@AFODC-611081928>
2016-12-09 23:03:51 +0800

    测试

--- src/invokespecial_vs_invokevirtual/InvokeTest.java
+++ src/invokespecial_vs_invokevirtual/InvokeTest.java
@@ -4,7 +4,7 @@ public class InvokeTest {
    public static void main( String args[] ) {  
 	   
 	      SubClass5 b = new SubClass5();  
-	      SuperClass5 supper = b;              //向上转型引用  
+	      SuperClass5 supper = b;              //向上转型引用   
 	      System.out.println(supper.method());//invokevirtual，当前引用的对象是b  
 	      supper.method();
 	      b.subMethod();  
